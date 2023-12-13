467b468450f51b32c4e2920db953d066098ffa87
Adam Alcott <aalcott14@gmail.com>
2023-12-13 17:12:42 -0600

    set up assets and player movements

---
+++ main.go
@@ -0,0 +1,56 @@
+package main
+
+import (
+	"game/assets"
+
+	"github.com/hajimehoshi/ebiten/v2"
+)
+
+type Vector struct {
+	X float64
+	Y float64
+}
+
+type Game struct {
+	playerPosition Vector
+}
+
+func (g *Game) Update() error {
+	speed := 5.0
+
+	if ebiten.IsKeyPressed(ebiten.KeyDown) {
+		g.playerPosition.Y += speed
+	}
+	if ebiten.IsKeyPressed(ebiten.KeyUp) {
+		g.playerPosition.Y -= speed
+	}
+	if ebiten.IsKeyPressed(ebiten.KeyLeft) {
+		g.playerPosition.X -= speed
+	}
+	if ebiten.IsKeyPressed(ebiten.KeyRight) {
+		g.playerPosition.X += speed
+	}
+
+	return nil
+}
+
+func (g *Game) Draw(screen *ebiten.Image) {
+	op := &ebiten.DrawImageOptions{}
+	op.GeoM.Translate(g.playerPosition.X, g.playerPosition.Y)
+	screen.DrawImage(assets.PlayerSprite, op)
+}
+
+func (g *Game) Layout(outsideWidth, outsideHeight int) (screenWidth, screenHeight int) {
+	return outsideWidth, outsideHeight
+}
+
+func main() {
+	g := &Game{
+		playerPosition: Vector{X: 100, Y: 100},
+	}
+
+	err := ebiten.RunGame(g)
+	if err != nil {
+		panic(err)
+	}
+}
