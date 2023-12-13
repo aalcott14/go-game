ebd05065013bfc93296aabf61632ed9fb54b7a42
Adam Alcott <aalcott14@gmail.com>
2023-12-13 17:37:03 -0600

    player package and rotation

--- game/game.go
+++ game/game.go
@@ -1,46 +1,33 @@
 package game
 
 import (
-	"game/assets"
-
 	"github.com/hajimehoshi/ebiten/v2"
 )
 
+const (
+	screenWidth  = 800
+	screenHeight = 600
+)
+
 type Game struct {
-	playerPosition Vector
+	player *Player
 }
 
 func NewGame() *Game {
-	g := &Game{
-		playerPosition: Vector{X: 100, Y: 100},
-	}
+	g := &Game{}
+	g.player = NewPlayer(g)
 
 	return g
 }
 
 func (g *Game) Update() error {
-	speed := 5.0
-
-	if ebiten.IsKeyPressed(ebiten.KeyDown) {
-		g.playerPosition.Y += speed
-	}
-	if ebiten.IsKeyPressed(ebiten.KeyUp) {
-		g.playerPosition.Y -= speed
-	}
-	if ebiten.IsKeyPressed(ebiten.KeyLeft) {
-		g.playerPosition.X -= speed
-	}
-	if ebiten.IsKeyPressed(ebiten.KeyRight) {
-		g.playerPosition.X += speed
-	}
+	g.player.Update()
 
 	return nil
 }
 
 func (g *Game) Draw(screen *ebiten.Image) {
-	op := &ebiten.DrawImageOptions{}
-	op.GeoM.Translate(g.playerPosition.X, g.playerPosition.Y)
-	screen.DrawImage(assets.PlayerSprite, op)
+	g.player.Draw(screen)
 }
 
 func (g *Game) Layout(outsideWidth, outsideHeight int) (screenWidth, screenHeight int) {
---
+++ game/player.go
@@ -0,0 +1,66 @@
+package game
+
+import (
+	"game/assets"
+	"math"
+
+	"github.com/hajimehoshi/ebiten/v2"
+)
+
+const (
+	rotationPerSecond = math.Pi
+)
+
+type Player struct {
+	game *Game
+
+	position Vector
+	rotation float64
+	sprite   *ebiten.Image
+}
+
+func NewPlayer(game *Game) *Player {
+	sprite := assets.PlayerSprite
+
+	bounds := sprite.Bounds()
+	halfW := float64(bounds.Dx()) / 2
+	halfH := float64(bounds.Dy()) / 2
+
+	pos := Vector{
+		X: screenWidth/2 - halfW,
+		Y: screenHeight/2 - halfH,
+	}
+
+	return &Player{
+		game:     game,
+		position: pos,
+		rotation: 0,
+		sprite:   sprite,
+	}
+}
+
+func (p *Player) Update() {
+	speed := rotationPerSecond / float64(ebiten.TPS())
+
+	if ebiten.IsKeyPressed(ebiten.KeyLeft) {
+		p.rotation -= speed
+	}
+	if ebiten.IsKeyPressed(ebiten.KeyRight) {
+		p.rotation += speed
+	}
+}
+
+func (p *Player) Draw(screen *ebiten.Image) {
+	bounds := p.sprite.Bounds()
+	halfW := float64(bounds.Dx()) / 2
+	halfH := float64(bounds.Dy()) / 2
+
+	op := &ebiten.DrawImageOptions{}
+	op.GeoM.Translate(-halfW, -halfH)
+	op.GeoM.Rotate(p.rotation)
+	op.GeoM.Translate(halfW, halfH)
+
+	op.GeoM.Translate(p.position.X, p.position.Y)
+
+	screen.DrawImage(p.sprite, op)
+}
